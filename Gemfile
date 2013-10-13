ea93ca23fe69bbb444d18228d1013abd43fb2298
verhe054 <verhe054@gmail.com>
2013-10-13 19:02:14 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '3.2.3'
+
+# Use sqlite3 as the database for Active Record
+group :development do
+	gem 'sqlite3', '1.3.5'
+end
+
+group :assets do
+	# Use SCSS for stylesheets
+	gem 'sass-rails', '3.2.4'
+	# Use Uglifier as compressor for JavaScript assets
+	gem 'uglifier', '1.2.3'
+	# Use CoffeeScript for .js.coffee assets and views
+	gem 'coffee-rails', '3.2.2'
+end
+
+gem 'jquery-rails'
+
+group :production do
+	gem 'pg', '0.12.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'd9bdec3f75fdbfe7abc6df0f35f5bb89a7ce07a17c33842d45aa933b9d68eb4a05881ca5d83dddfe2b89fdee5387df132c357c8d03f3d0f143c2415936aa51f6'
